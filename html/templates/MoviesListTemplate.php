8c8354cc2c882423a09ffc5fdfccf393838aae82
andrea1192 <andrea1192@users.noreply.github.com>
2024-07-24 13:21:52 +0200

    Refactoring (1/2): spostamento template
    Analogo a ccedf45

--- html/templates/MoviesListTemplate.php
+++ html/templates/MoviesListTemplate.php
@@ -1,18 +1,11 @@
-<?php namespace templates;
-
-	require_once('views/MoviesView.php');
-
-	$view = new \views\MoviesView();
-
-	// TODO: add XML prologue (xml version="1.0" encoding="UTF-8")
-?>
+<?php /* TODO: add XML prologue (xml version="1.0" encoding="UTF-8") */ ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 	<head>
-		<title><?php $view->printTitle() ?></title>
+		<title><?php $this->printTitle() ?></title>
 
 		<link rel="stylesheet" href="../css/common.css" type="text/css" />
 	</head>
@@ -21,7 +14,7 @@
 		<div id="header">Header</div>
 
 		<div id="content" class="wrapper">
-			<?php $view->printList() ?>
+			<?php $this->printList() ?>
 
 			<button class="fab new_post"><span class="material-symbols-outlined"></span><span class="label">Add movie</span></button>
 		</div>
