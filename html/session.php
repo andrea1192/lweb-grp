96f4f17ba6bb3fcdf8f6577d5b127f6c742b7749
andrea1192 <andrea1192@users.noreply.github.com>
2024-07-25 12:12:17 +0200

    Metodi gestione sessione
    Aggiunti metodi per determinare se utente corrente è registrato, mod, admin...

--- html/session.php
+++ html/session.php
@@ -1,8 +1,30 @@
 <?php namespace controllers;
 
 	class MockSession {
+		private $username = 'bar';
+		private $privilege = 2;
+
+		public function getUsername() {
+			return $this->username;
+		}
 
 		public function isLoggedIn() {
-			return true;
+			return isset($this->username);
+		}
+
+		public function isRegistered() {
+			return (bool) ($this->privilege >= 1);
+		}
+
+		public function isMod() {
+			return (bool) ($this->privilege >= 2);
+		}
+
+		public function isAdmin() {
+			return (bool) ($this->privilege == 3);
+		}
+
+		public function isAuthor($object) {
+			return (bool) ($this->username == $object->author);
 		}
 	}
