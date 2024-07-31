3cdbea27570fc4271683568304d60b3746c07c0c
andrea1192 <andrea1192@users.noreply.github.com>
2024-07-31 17:44:27 +0200

    Metodi gestione sessione
    Aggiunti metodi per ricevere tipo utente e livello di reputazione corrente

--- html/session.php
+++ html/session.php
@@ -3,9 +3,21 @@
 	class MockSession {
 		private $username = 'bar';
 		private $privilege = 2;
+		private $reputation = 42;
 
 		public function getUsername() {
 			return $this->username;
+		}
+
+		public function getUserType() {
+
+			switch ($this->privilege) {
+				case 0: return 'Banned';
+				default:
+				case 1: return 'User';
+				case 2: return 'Moderator';
+				case 3: return 'Administrator';
+			}
 		}
 
 		public function isLoggedIn() {
@@ -26,5 +38,9 @@
 
 		public function isAuthor($object) {
 			return (bool) ($this->username == $object->author);
+		}
+
+		public function getReputation() {
+			return $this->reputation;
 		}
 	}
