9ad0c8f6082b710fb895c6bc640af9d2c36ed69f
andrea1192 <andrea1192@users.noreply.github.com>
2024-08-13 19:00:55 +0200

    Fix: regression generazione URL
    Corretta action nei link per MovieEditView

--- html/views/Movie.php
+++ html/views/Movie.php
@@ -17,7 +17,7 @@
 			switch ($action) {
 				default:
 				case 'display': return "movie.php?id={$this->movie->id}";
-				case 'edit': return "movie.php?id={$this->movie->id}&action=add";
+				case 'edit': return "movie.php?id={$this->movie->id}&action=edit";
 			}
 		}
 
